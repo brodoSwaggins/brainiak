a40a8288e838935fb0f47cb42d066f9ead716b74
Isaac_Ash <izikasch@gmail.com>
2024-08-20 17:40:56 +0300

    [eventSeg2] HMM piepline, find best HMM num events

--- docs/examples/eventseg/eventSeg2.py
+++ docs/examples/eventseg/eventSeg2.py
@@ -35,6 +35,60 @@ def getBoolMask(atlas, labels, map=None):
     bool_mask = reduce(lambda x, y: x + y, [(map.get_fdata() == i) for i in label_index])
     mask_img = nl.image.new_img_like(map, bool_mask)
     return mask_img, bool_mask
+
+def within_across_corr(D, nEvents, w=5, nPerm=1000, verbose=0, rsd = 0):
+    """
+    Compute within vs across boundary correlations for a given number of events
+    Parameters
+    ----------
+    D : np array (nSubj x nVertices x nTR)
+    nEvents : for HMM
+    w : time window for time autocorrelation
+    nPerm : number of permutations for statistical analysis
+    verbose : 0 - no print, 1 - print log, 2 - print log and output violin plot
+    Returns
+    -------
+    within_across : np array (nSubj x nPerm+1)
+    """
+    nSubj, _,  nTR = D.shape
+    within_across = np.zeros((nSubj, nPerm+1))
+    print(".......... Computing time correlation for HMM with {} events.............".format(nEvents))
+    for left_out in range(nSubj):
+        # Fit to all but one subject
+        ev = brainiak.eventseg.event.EventSegment(nEvents)
+        ev.fit(D[np.arange(nSubj) != left_out,:,:].mean(0).T)
+        events = np.argmax(ev.segments_[0], axis=1)
+
+        # TEST: Compute correlations separated by w in time for the held-out subject
+        corrs = np.zeros(nTR-w)
+        for t in range(nTR-w):
+            corrs[t] = pearsonr(D[left_out,:,t],D[left_out,:,t+w])[0]
+        _, event_lengths = np.unique(events, return_counts=True)
+
+        # Compute mean
+        # within vs across boundary correlations, for real and permuted bounds
+        np.random.seed(rsd)
+        for p in range(nPerm+1): # p=0 is the real events (no permuataion)
+            within = corrs[events[:-w] == events[w:]].mean()
+            across = corrs[events[:-w] != events[w:]].mean()
+            within_across[left_out, p] = within - across
+            # This makes the next itertion run over a permuted version of the event lengths
+            perm_lengths = np.random.permutation(event_lengths)
+            events = np.zeros(nTR, dtype=int)
+            events[np.cumsum(perm_lengths[:-1])] = 1
+            events = np.cumsum(events)
+        if verbose >0:
+            print('Subj ' + str(left_out+1) + ': WvsA = ' + str(within_across[left_out,0]))
+    if verbose > 1:
+        plt.figure(figsize=(1.5, 5))
+        plt.violinplot(within_across[:, 1:].mean(0), showextrema=True)  # permuted
+        plt.scatter(1, within_across[:, 0].mean(0))  # real
+        plt.gca().xaxis.set_visible(False)
+        plt.ylabel('Within vs across boundary correlation')
+        plt.title('{} {} :\nHeld-out subject HMM with {} events ({} perms)'.format(side, label, nEvents, nPerm))
+        plt.show(block=blck)
+    return within_across
+
 #%% Example use - single subject
 if sys.platform == 'linux':
     file = r'/home/itzik/Desktop/EventBoundaries/recall_files/sherlock_recall_s1.nii'
@@ -311,7 +365,7 @@ files = [nl.image.load_img(p) for p in paths]
 ## todo ............. acces the preprocessed data
 #%% Load MilkyWay preprocessed data
 if sys.platform == 'linux':
-    pathDS = r'smb://132.64.186.144/hartlabnas/personal_folders/isaac.ash/OptCodingEB/narrative_DS/ds002345-download'
+    pathDS = r'/home/itzik/Desktop/EventBoundaries/milkyway_vodka/Milkyway/niftis_preprocessed'
 else:
     pathDS = r'C:\Users\izika\OneDrive\Documents\Hebrew U\Modeling of cognition\brainiak\docs\examples\eventseg\4\milkyway_vodka\Milkyway\niftis_preprocessed'
 file_names = [pathDS + '/' + f for f in os.listdir(pathDS) if f.endswith('.nii') and not f.startswith('.')]
@@ -327,12 +381,14 @@ print(all_TR.shape)
 first_TR = image.index_img(ffz, 0)
 print(first_TR.shape)
 plotting.plot_stat_map(first_TR, threshold=1)#, output_file=output_dir / "first_TR.png")
+plt.show(block = blck)
 #%% Z score all data
 masker = nl.maskers.MultiNiftiMasker(standardize=True)
 BOLD = masker.fit_transform(file_names)
 BOLD = masker.inverse_transform(BOLD)
 #%% First subject the same?
-plotting.plot_stat_map(image.index_img(BOLD[1], 10), threshold=1)#, output_file=output_dir / "first_TR.png")
+plotting.plot_stat_map(image.index_img(BOLD[1], 0), threshold=1)#, output_file=output_dir / "first_TR.png")
+plt.show(block = blck)
 #%% Project to cortical surface
 side = 'left'
 fsaverage = datasets.fetch_surf_fsaverage()
@@ -353,7 +409,7 @@ for i in range(1, 4, 1):
         #cmap='plasma',
         threshold=0.0001,
     )
-    plt.show(block=False)
+    plt.show(block=blck)
     plt.pause(3)
     plt.close()
 
@@ -376,11 +432,25 @@ plotting.plot_surf_roi(
     cmap='plasma',
     threshold=0.0001,
 )
+plt.show(block=blck)
+#%% Find the best numbr of events for the region
+all_surf = np.array(surfL) if side == 'left' else np.array(surfR)
+all_region = all_surf[:,regionInd,:]
+num_events = np.arange(2, 50, 1)
+score = [] ; nPerm = 1000 ; w = 5 ; nSubj = len(files)
+within_across_all = np.zeros((len(num_events),nSubj, nPerm+1))
+for i,nEvents in enumerate(num_events):
+    within_across_all[i] = within_across_corr(all_region, nEvents, w, nPerm, verbose=0)
+    score.append(within_across_all[i,:,0].mean())
+    print(f"Number of regions: {nEvents}, score: {score[-1]}")
+#%% plot the results
+plt.plot(num_events, score); plt.show(block = blck)
+
 #%%
 all_surf = np.array(surfL) if side == 'left' else np.array(surfR)
 all_region = all_surf[:,regionInd,:]
 w = 5  # window size
-nEvents = 60
+nEvents = 20
 nPerm = 1000 # number of random permutations
 nSubj = len(files) ; nTR = all_region.shape[2]
 within_across = np.zeros((nSubj, nPerm+1))
@@ -416,6 +486,7 @@ plt.scatter(1, within_across[:,0].mean(0)) # real
 plt.gca().xaxis.set_visible(False)
 plt.ylabel('Within vs across boundary correlation')
 plt.title('{} {} :\nHeld-out subject HMM with {} events ({} perms)'.format(side, label, nEvents, nPerm))
+plt.show(block = blck)
 #%%
 fsaverage = datasets.fetch_surf_fsaverage()
 atlas_destrieux = datasets.fetch_atlas_surf_destrieux()
